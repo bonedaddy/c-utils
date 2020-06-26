341fe71629abedbf4aa4e24a8c942996bbdbf3fb
postables <postables@rtradetechnologies.com>
2020-06-25 17:12:19 -0700

    some more misc stuff

--- misc.c
+++ misc.c
@@ -1,6 +1,25 @@
-// this syntax lets us define a char array with 1000 elements
-const char c[] = {[0] = 'c', [1000] = 'ccc'};
+#include <stdio.h>
+// the ".." indicates to import from local directory
+#include "array_len.h"
 
+// this syntax lets us define a char array with 1000 elements
+const char c[] = {[0] = 'c', [1000] = '\n'};
+int alter(int ii[]);
 int main(void) {
     printf("%c\n", c[999]);
+    int i[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    alter(i);
+    if (i[9] != 0) {
+        printf("not zero");
+        return -1;
+    }
+    return 0;
+}
+
+int alter(int ii[]) {
+    if (array_lens(ii) > 10) {
+        return -1;
+    }
+    ii[9] = 0;
+    return 0;
 }
--- random.c
+++ random.c
@@ -3,6 +3,7 @@
 #include <time.h>
 
 int getRandomNumber(int lower, int upper);
+char * getRandomString(int stringLength);
 
 // defines a constant character array
 const char letters[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
@@ -11,9 +12,17 @@ int main(void) {
     // seed random number generate using time
     srand(time(0));
     printf("%c\n", letters[getRandomNumber(0, 25)]);    
+    printf("%s\n", getRandomString(10));
     return 0;
 }
 
+char * getRandomString(int stringLength) {
+    char *word;
+    for (int i = 0; i < stringLength; i++) {
+        word += letters[getRandomNumber(0, 25)];
+    }
+    return word;
+}
 
 int getRandomNumber(int lower, int upper) {
     return (rand() % (upper - lower + 1)) + lower;
